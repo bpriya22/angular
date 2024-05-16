feaa141e4e359f90cd675b6aff130a60f7dc37ca
rrm <priyanka.balakrishna2@gmail.com>
2024-05-16 08:45:25 +0530

    interpolation and property-binding

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -3,11 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
  standalone: true,
-  template: `<h2>Welcome Priya and {{name}}</h2>`,
+  template: `<h2>Welcome Priya and {{name}}</h2>
+  <input [id]="myid" type="text" value="Rahul">//Property Binding
+  <input id="{{myid}}" type="text" value="Rahul" [disabled]="isdisabled">//with '[]'
+  <input id="{{myid}}" type="text" value="Rahul" bind-disabled="isdisabled">//with 'bind'
+
+  `,
   styles: []
 })
 export class TestComponent implements OnInit{
-  public name="Rahul";
+  public name="Rahul";//interpolation-simplest way to bind data from class to template.
+  public myid = "testId"
+  public isdisabled = true
   constructor(){}
   ngOnInit() {
   
